635f8ff0b433e3e2583d7656d59a8dcb5c4f7d23
Rakin Ishraq <rakinishraqm@gmail.com>
2022-02-09 03:16:50 -0500

    fix music player

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-$player = $("#player");
-$player.prop("volume", "0.1");
+var interval;
+var off = true;
+
+$player = new Audio("music.mp3");
+$player.volume = 0.1;
+$note = $("#note");
+$note.prop("opacity", "0.2");
 
 function toggle_music() {
-	$note = $("#note");
-	vol = $player.prop("volume");
+	$player.play();
 
-	//if (vol == 1) $player.animate({volume: 0}, 2000, "linear");
-	//else if (vol == 0) $player.animate({volume: 1}, 2000, "linear");
-	$note.animate({opacity: vol==1 ? 0.2 : 0.7}, 500, "linear");
-	$player.animate({volume: vol==1 ? 0 : 0.1}, 2000, "linear");
+	vol = $player.volume;
+	off = !off;
+	$note.animate({opacity: off ? 0.2 : 0.7}, 500, "linear");
+	//$player.animate({volume: vol==0.1 ? 0 : 0.1}, 2000, "linear");
+	interval = setInterval(function () {fade(off ? 0 : 0.1)}, 200);
+}
+
+function fade(target) {
+	if (Math.abs(target - vol) < 0.01) {
+		vol = target;
+		clearInterval(interval);
+	}
+	else {
+		vol += Math.sign(target - vol) * 0.005;
+	}
+	$player.volume = vol;
 }
