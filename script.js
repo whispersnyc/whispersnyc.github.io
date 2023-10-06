698bbc73b3763f1c1bad2fbd6f64c343127efce7
User <email@email.com>
2023-10-06 04:00:40 -0400

    konami code (credit Ehsan Kia)

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ var options = {
   "grainHeight": 1
 };
 grained("#grain-container", options);
+
+
+let cursor = 0; // credit Ehsan Kia
+const KONAMI_CODE = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+document.addEventListener('keydown', (e) => {
+  cursor = (e.keyCode == KONAMI_CODE[cursor]) ? cursor + 1 : 0;
+  if (cursor == KONAMI_CODE.length) activate();
+});
